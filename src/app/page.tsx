9008c63b8f0c50671dbc2717207ed08120e5c7de
Teba_eleven <73474645+Tebaeleven@users.noreply.github.com>
2025-01-10 01:01:27 +0900

    NextTopLoaderの導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <div>
-      <Button>Click me</Button>
+      <Button onClick={() => router.push("/editor/123")}>Go to Editor</Button>
     </div>
   );
 }
